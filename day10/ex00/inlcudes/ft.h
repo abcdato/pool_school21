2c5c08a9c99d4918734e1e1612b7b0651c12502c
abcdato <42653648+abcdato@users.noreply.github.com>
2020-05-31 21:16:57 +0300

    Add files via upload

---
+++ day10/ex00/inlcudes/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmasala <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/10 14:33:43 by tmasala           #+#    #+#             */
+/*   Updated: 2020/02/10 15:05:28 by tmasala          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
