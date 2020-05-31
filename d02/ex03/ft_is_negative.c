bf6f95ed137d0bb02d8d295bd6ed00d1d56fb74d
abcdato <42653648+abcdato@users.noreply.github.com>
2020-05-31 18:30:02 +0300

    Update ft_is_negative.c

--- d02/ex03/ft_is_negative.c
+++ d02/ex03/ft_is_negative.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putchar(char c)
+void	ft_putchar(char c);
 
 void	ft_is_negative(int n)
 {
